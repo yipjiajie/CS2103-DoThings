2aa5eb13734d8a61be2e86f7a4a76c84153c91ad
John <johnlimed@gmail.com>
2014-02-26 17:48:06 +0800

    edited Parser.java to include switch.

--- src/Parser.java
+++ src/Parser.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class Parser {
 	private static String input;
-	private static String command;
 	private static ArrayList<String> parsedInput;
 
 	public Parser(String userInput) {
@@ -10,11 +9,42 @@ public class Parser {
 	}
 	
 	enum CommandType {
-		ADD, DELETE, DISPLAY, CLEAR, SORT, SEARCH, EXIT, INVALID;
+		ADD, DELETE, DISPLAY, CLEAR, SORT, SEARCH, EXIT, INVALID, UPDATE;
 	}
-
+	private static CommandType getCommandType(String com){
+		if ( com.equalsIgnoreCase("\\add") || com.equalsIgnoreCase("\\a") ) {
+			return CommandType.ADD;
+		} else if ( com.equalsIgnoreCase("\\list") || com.equalsIgnoreCase("\\l") ) {
+			return CommandType.DISPLAY;
+		} else if ( com.equalsIgnoreCase("\\update") || com.equalsIgnoreCase("\\u") ) {
+			return CommandType.UPDATE;
+		} else if ( com.equalsIgnoreCase("\\delete") || com.equalsIgnoreCase("\\d") ) {
+			return CommandType.DELETE;
+		} else {
+			return CommandType.INVALID;
+		} 
+	}
 	protected static void parseCommands() {
 		parsedInput = new ArrayList<String>(Arrays.asList(input.split(" ", 2)));
-		command = parsedInput.get(0); 
+		String command = parsedInput.get(0); 
+		CommandType commandType = getCommandType(command);
+
+		switch (commandType) {
+			case ADD:
+				//add
+				break;
+			case DISPLAY:
+				//display
+				break;
+			case UPDATE:
+				//call edit function
+				break;
+			case DELETE:
+				//call delete function
+				break;
+			case: INVALID:
+				//print error msg
+			}
+		}
 	}
 }
