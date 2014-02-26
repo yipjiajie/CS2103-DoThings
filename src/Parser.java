2a78ed986f82a350174c65693e6577f9bd476fcc
John <johnlimed@gmail.com>
2014-02-26 17:32:14 +0800

    Addressed issues and errors in both java files.

--- src/Parser.java
+++ src/Parser.java
@@ -1,32 +1,19 @@
-import java.util.*
+import java.util.*;
 
 public class Parser {
-	private String input;
-	private String command;
-	private ArrayList<String> parsedInput;
+	private static String input;
+	private static String command;
+	private static ArrayList<String> parsedInput;
 
-	public Parser(String input) {
-		this.input = input;
+	public Parser(String userInput) {
+		input = userInput;
 	}
 	
 	enum CommandType {
 		ADD, DELETE, DISPLAY, CLEAR, SORT, SEARCH, EXIT, INVALID;
 	}
 
-
-	public static void main(String[] args) {
-		checkArguments(args.length);
-		checkFile(args[0]);
-		showToUser(String.format(MESSAGE_WELCOME, FILE_NAME));
-		while(true) {
-			String userCommand = readUserCommand();
-			String feedback = executeCommand(userCommand);
-			showToUser(feedback);
-			saveFile();
-		}
-	}
-
-	private static void parseCommands() {
+	protected static void parseCommands() {
 		parsedInput = new ArrayList<String>(Arrays.asList(input.split(" ", 2)));
 		command = parsedInput.get(0); 
 	}
