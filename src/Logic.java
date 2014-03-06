c5b63ecca3c818b4894067be53e84ef2a2d6a5e4
john <john@localhost>
2014-03-06 22:49:30 +0800

    edited Logic and Parser. Included firstStep in logic to check for txt file and get parser to parse commands.
    Added ParseCommand in parser.

--- src/Logic.java
+++ src/Logic.java
@@ -38,25 +38,147 @@ public class Logic{
 	private static ArrayList<ArrayList<String>> customCommandList;
 	private static Stack<ArrayList<Task>> taskUndoStack;
 	private static Stack<ArrayList<ArrayList<String>>> commandUndoStack;
-	
-	// not needed
-	/*
-	protected static Task createTask(String description, Calendar startOfTask, Calendar endOfTask) {
-		Task task = new Task(description, startOfTask, endOfTask);
-		return task;
-	}
-	*/
+	/////////////////////////////////////////////////
+	/////////////////////////////////////////////////
+	/////////////////////////////////////////////////
+	private static final String DEFAULT_ADD = "\\add";
+	private static final String DEFAULT_ADD2 = "\\a";
+	private static final String DEFAULT_UPDATE = "\\update";
+	private static final String DEFAULT_UPDATE2 = "\\u";
+	private static final String DEFAULT_DELETE = "\\delete";
+	private static final String DEFAULT_DELETE2 = "\\d";
+	private static final String DEFAULT_LIST = "\\list";
+	private static final String DEFAULT_LIST2 = "\\l";
+	private static final String DEFAULT_UNDO = "\\undo";
+	private static final String DEFAULT_UNDO2 = "\\un";
+	private static final String DEFAULT_CUSTOM = "\\custom";
+	private static final String DEFAULT_CUSTOM2 = "\\cc";
+	private static final String DEFAULT_DELETE_CUSTOM = "\\dcustom";
+	private static final String DEFAULT_DELETE_CUSTOM2 = "\\dc";
+	private static final String DEFAULT_HELP = "\\help";
+	private static final String DEFAULT_HELP2 = "\\h";
+	private static final String DEFAULT_SEARCH = "\\search";
+	private static final String DEFAULT_SEARCH2 = "\\s";
+	private static final String DEFAULT_EXIT = "\\exit";
+	private static final String DEFAULT_EXIT2 = "\\e";
+
+	public static final String MESSAGE_INVALID_DELETE = " is an invalid task number, cannot delete.";
+	public static final String MESSAGE_DELETE_SUCCESS = "Successfuly deleted task number ";
+		private enum CommandType {
+		ADD, DELETE, UPDATE, LIST, UNDO, SEARCH, CUSTOM, DELETE_CUSTOM, HELP, EXIT, INVALID;
+	}	
+
+	private static CommandType getCommandType(String com) {
+		ArrayList<ArrayList<String>> customCommand = DiskIO.readCustomCommands();
+		
+		if (com.equals(DEFAULT_ADD) || com.equals(DEFAULT_ADD2) || customCommand.get(DiskIO.ADD_INDEX).contains(com)) {
+			return CommandType.ADD;
+		} else if (com.equals(DEFAULT_LIST) || com.equals(DEFAULT_LIST2) || customCommand.get(DiskIO.LIST_INDEX).contains(com)) {
+			return CommandType.LIST;
+		} else if (com.equals(DEFAULT_UPDATE) || com.equals(DEFAULT_UPDATE2) || customCommand.get(DiskIO.UPDATE_INDEX).contains(com)) {
+			return CommandType.UPDATE;
+		} else if (com.equals(DEFAULT_DELETE) || com.equals(DEFAULT_DELETE2) || customCommand.get(DiskIO.DELETE_INDEX).contains(com)) {
+			return CommandType.DELETE;
+		} else if (com.equals(DEFAULT_UNDO) || com.equals(DEFAULT_UNDO2) || customCommand.get(DiskIO.UNDO_INDEX).contains(com)) {
+			return CommandType.UNDO;
+		} else if (com.equals(DEFAULT_CUSTOM) || com.equals(DEFAULT_CUSTOM2) || customCommand.get(DiskIO.CUSTOM_INDEX).contains(com)) {
+			return CommandType.CUSTOM;
+		} else if (com.equals(DEFAULT_HELP) || com.equals(DEFAULT_HELP2) || customCommand.get(DiskIO.HELP_INDEX).contains(com)) {
+			return CommandType.HELP;
+		} else if (com.equals(DEFAULT_EXIT) || com.equals(DEFAULT_EXIT2) || customCommand.get(DiskIO.EXIT_INDEX).contains(com)) {
+			return CommandType.EXIT;
+		} else if (com.equals(DEFAULT_SEARCH) || com.equals(DEFAULT_SEARCH2) || customCommand.get(DiskIO.SEACRH_INDEX).contains(com)) {
+			return CommandType.SEARCH;
+		} else {
+			return CommandType.INVALID;
+		}
+	} 
+
 	protected static String firstStep (String userInput) {
 		String message;
 		String cmd;
+		String[] infoFromParser = new String[2];
 		checkTxtFile();
-		parseCommand(userInput);
+		infoFromParser = parseCommand(userInput);
+
+		CommandType commandType = getCommandType(infoFromParser[0]);
+		String userDescription = infoFromParser[1];
+		
+		switch (commandType) {
+			case ADD:
+				addTasks(userDescription);
+				break;
+			case LIST:
+				listTasks(userDescription);
+				break;
+			case UPDATE:
+				//
+				break;
+			case DELETE:
+				deleteTasks(userDescription);
+				break;
+			case HELP:
+				displayHelp();
+				break;
+			case CUSTOM:
+				//
+				break;
+			case DELETE_CUSTOM:
+				//
+				break;
+			case UNDO:
+				undoActions(userDescription);
+				break;
+			case SEARCH:
+				//
+				break;
+			case EXIT:
+				return true;
+			default:
 
 		return message;
 	}
 
 	private static void checkTxtFile() {
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 	private static void executeAdd(Task task) {
--- src/MainParser.java
+++ src/MainParser.java
@@ -5,60 +5,12 @@ import java.util.Date;
 //TODO : ADD, UPDATE, SEARCH, CUSTOM, HELP
 
 class MainParser {
-	private static final String DEFAULT_ADD = "\\add";
-	private static final String DEFAULT_ADD2 = "\\a";
-	private static final String DEFAULT_UPDATE = "\\update";
-	private static final String DEFAULT_UPDATE2 = "\\u";
-	private static final String DEFAULT_DELETE = "\\delete";
-	private static final String DEFAULT_DELETE2 = "\\d";
-	private static final String DEFAULT_LIST = "\\list";
-	private static final String DEFAULT_LIST2 = "\\l";
-	private static final String DEFAULT_UNDO = "\\undo";
-	private static final String DEFAULT_UNDO2 = "\\un";
-	private static final String DEFAULT_CUSTOM = "\\custom";
-	private static final String DEFAULT_CUSTOM2 = "\\cc";
-	private static final String DEFAULT_DELETE_CUSTOM = "\\dcustom";
-	private static final String DEFAULT_DELETE_CUSTOM2 = "\\dc";
-	private static final String DEFAULT_HELP = "\\help";
-	private static final String DEFAULT_HELP2 = "\\h";
-	private static final String DEFAULT_SEARCH = "\\search";
-	private static final String DEFAULT_SEARCH2 = "\\s";
-	private static final String DEFAULT_EXIT = "\\exit";
-	private static final String DEFAULT_EXIT2 = "\\e";
+	protected static String[] parseCommand(String userInput) {
+		String[] userCommand =new String[2];
+		userCommand = Arrays.asList(userInput.split(" ", 2));
+		return userCommand;
+	}
 
-	public static final String MESSAGE_INVALID_DELETE = " is an invalid task number, cannot delete.";
-	public static final String MESSAGE_DELETE_SUCCESS = "Successfuly deleted task number ";
-
-	private enum CommandType {
-		ADD, DELETE, UPDATE, LIST, UNDO, SEARCH, CUSTOM, DELETE_CUSTOM, HELP, EXIT, INVALID;
-	}	
-
-	private static CommandType getCommandType(String com) {
-		ArrayList<ArrayList<String>> customCommand = DiskIO.readCustomCommands();
-		
-		if (com.equals(DEFAULT_ADD) || com.equals(DEFAULT_ADD2) || customCommand.get(DiskIO.ADD_INDEX).contains(com)) {
-			return CommandType.ADD;
-		} else if (com.equals(DEFAULT_LIST) || com.equals(DEFAULT_LIST2) || customCommand.get(DiskIO.LIST_INDEX).contains(com)) {
-			return CommandType.LIST;
-		} else if (com.equals(DEFAULT_UPDATE) || com.equals(DEFAULT_UPDATE2) || customCommand.get(DiskIO.UPDATE_INDEX).contains(com)) {
-			return CommandType.UPDATE;
-		} else if (com.equals(DEFAULT_DELETE) || com.equals(DEFAULT_DELETE2) || customCommand.get(DiskIO.DELETE_INDEX).contains(com)) {
-			return CommandType.DELETE;
-		} else if (com.equals(DEFAULT_UNDO) || com.equals(DEFAULT_UNDO2) || customCommand.get(DiskIO.UNDO_INDEX).contains(com)) {
-			return CommandType.UNDO;
-		} else if (com.equals(DEFAULT_CUSTOM) || com.equals(DEFAULT_CUSTOM2) || customCommand.get(DiskIO.CUSTOM_INDEX).contains(com)) {
-			return CommandType.CUSTOM;
-		} else if (com.equals(DEFAULT_HELP) || com.equals(DEFAULT_HELP2) || customCommand.get(DiskIO.HELP_INDEX).contains(com)) {
-			return CommandType.HELP;
-		} else if (com.equals(DEFAULT_EXIT) || com.equals(DEFAULT_EXIT2) || customCommand.get(DiskIO.EXIT_INDEX).contains(com)) {
-			return CommandType.EXIT;
-		} else if (com.equals(DEFAULT_SEARCH) || com.equals(DEFAULT_SEARCH2) || customCommand.get(DiskIO.SEACRH_INDEX).contains(com)) {
-			return CommandType.SEARCH;
-		} else {
-			return CommandType.INVALID;
-		}
-	} 
-	
 	// returns true only on exit command
 	public static boolean parse(String code) {
 		ArrayList<String> tokens = new ArrayList<String>(Arrays.asList(code.split(" ", 2)));
