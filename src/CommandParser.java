da8b138901a5490368b38a339ee7f7d4fb673ff3
John <johnlimed@gmail.com>
2014-03-26 13:54:56 +0800

    Merge

--- src/CommandParser.java
+++ src/CommandParser.java
@@ -45,8 +45,8 @@ class CommandParser {
 	
 	/**
 	 * Returns an array of string of length 4 containing the start and end
-	 * dates and times of the input. Index 0 and 2 contains the start date
-	 * and time respectively. Index 1 and 3 contains the end date and time.
+	 * dates and times of the input. Index 0 and 1 contains the start date
+	 * and time respectively. Index 2 and 3 contains the end date and time.
 	 * @param input
 	 * @return a String array containing start and end date/times
 	 */
@@ -64,7 +64,7 @@ class CommandParser {
 	 * @param fields
 	 * @return a String array containing the start and end date
 	 */
-	protected static String[] getDateFields(ArrayList<String> input, String[] fields) {
+	private static String[] getDateFields(ArrayList<String> input, String[] fields) {
 		for (int i = 0; i < input.size(); i++) {
 			if (DateParser.isDate(input.get(i))) {
 				if (fields[Task.START_DATE] == null) {
@@ -86,7 +86,7 @@ class CommandParser {
 	 * @param fields
 	 * @return a String array containing the start and end date
 	 */
-	protected static String[] getTimeFields(ArrayList<String> input, String[] fields) {
+	private static String[] getTimeFields(ArrayList<String> input, String[] fields) {
 		for (int i = 0; i < input.size(); i++) {
 			if (TimeParser.isTime(input.get(i))) {
 				if (fields[Task.START_TIME] == null) {
--- src/DateParser.java
+++ src/DateParser.java
@@ -14,15 +14,13 @@ public class DateParser{
 	private static final String DATE_FORMAT_5 = "dd.MMM.YYYY";
 	private static final String DATE_FORMAT_6 = "dd/MM";
 	private static final String DATE_FORMAT_7 = "dd-MM";
-	private static final String DATE_FORMAT_8 = "dd.MM";
-	private static final String DATE_FORMAT_9 = "dd/MMM";
-	private static final String DATE_FORMAT_10 = "dd-MMM";
-	private static final String DATE_FORMAT_11 = "dd.MMM";
-	private static final String DATE_FORMAT_12 = "dd MMM";
-	private static final String DATE_FORMAT_13 = "E";
+	private static final String DATE_FORMAT_8 = "dd/MMM";
+	private static final String DATE_FORMAT_9 = "dd-MMM";
+	private static final String DATE_FORMAT_10 = "dd.MMM";
+	private static final String DATE_FORMAT_11 = "E";
 	private static final int FORMAT_TYPE_0_INDEX = 6;	//format 0 : full date, with year
-	private static final int FORMAT_TYPE_1_INDEX = 13;	//format 1 : with day and month only
-	private static final int FORMAT_TYPE_2_INDEX = 14;	//format 2 : day of the week, Friday, etc.
+	private static final int FORMAT_TYPE_1_INDEX = 11;	//format 1 : with day and month only
+	private static final int FORMAT_TYPE_2_INDEX = 12;	//format 2 : day of the week, Friday, etc.
 	
 	private static final String DATE_TODAY = "today";
 	private static final String DATE_TOMORROW = "tomorrow";
@@ -41,9 +39,7 @@ public class DateParser{
 			DateTimeFormat.forPattern(DATE_FORMAT_8), 
 			DateTimeFormat.forPattern(DATE_FORMAT_9), 
 			DateTimeFormat.forPattern(DATE_FORMAT_10), 
-			DateTimeFormat.forPattern(DATE_FORMAT_11), 
-			DateTimeFormat.forPattern(DATE_FORMAT_12), 
-			DateTimeFormat.forPattern(DATE_FORMAT_13)
+			DateTimeFormat.forPattern(DATE_FORMAT_11)
 		));
 
 	
@@ -216,12 +212,9 @@ public class DateParser{
 			tokens = date.split("/");
 		} else if (date.contains("-")) {
 			tokens = date.split("-");
-		} else if (date.contains(".")){
-			tokens = date.split(".");
-		} else {
-			tokens = date.split(" ");
 		}
 		
+		System.out.println(tokens.length);
 		int[] intTokens = new int[tokens.length];
 		for (int i = 0; i < tokens.length; i++) {
 			try {	
