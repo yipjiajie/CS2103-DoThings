ace82d474ee63d66bed30556b2ff4e95be386afd
Lewis <Lewis@localhost>
2014-04-02 12:54:47 +0800

    Added tests for add, update, delete

---
+++ src/TestCommands.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+public class TestCommands {
+
+	@Test
+	public void test() {
+		MainLogic.runLogic("delete all");
+		ArrayList<Task> list = Task.getList();
+		assertTrue(list.isEmpty());
+		
+		MainLogic.runLogic("add buy milk");
+		list = Task.getList();
+		assertTrue(list.get(0).getDescription().equals("buy milk"));
+		
+		MainLogic.runLogic("update 1 alias milk");
+		list = Task.getList();
+		assertTrue(list.get(0).getAlias().equals("milk"));
+		
+		MainLogic.runLogic("update milk desc eat cereal");
+		list = Task.getList();
+		assertTrue(list.get(0).getDescription().equals("eat cereal"));
+		
+		MainLogic.runLogic("delete milk");
+		list = Task.getList();
+		assertTrue(list.isEmpty());
+		
+		MainLogic.runLogic("undo");
+		list = Task.getList();
+		assertFalse(list.isEmpty());
+	}
+
+}
